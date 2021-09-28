f8d7525cfce3756dd6135d9eaa7de2783c4d5a66
Al-1117 <alioune1117@gmail.com>
2021-09-28 07:13:17 +0200

    Add files via upload
    fix URL

---
+++ web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'App\Http\Controllers\Api\ApiController@create');
+Route::get('/show/{id}', 'App\Http\Controllers\Api\ApiController@show');
+
+
+// Route for API
+
+Route::get('/ajax-request', 'App\Http\Controllers\Api\ApiController@store');
+//Route::get('/test', 'App\Http\Controllers\Api\ApiController@controllerMethod');
+
